fa7a072d4c10bac59004b8889c642de85e8fbb23
DianaHismatullina <dianarh@rambler.ru>
2016-12-29 07:06:18 +0300

    Update Homework_One.cpp

--- Homework_One.cpp
+++ Homework_One.cpp
@@ -8,68 +8,57 @@
 #include <time.h>
 
 
-unsigned right(unsigned a, int n) //  n - размер блока в битах
+unsigned right(unsigned a, int n)
 {
-	// a- исходное значение
-	unsigned maska = 0; // Маска для получения правых L битов (младшие L разрядов должны быть 1)
+	unsigned maska = 0; 
 	for (int i = 0; i<3; i++)
 	{
-		maska <<= 1; // Сдвиг влево на 1 бит
-		maska += 1; // Младший бит в 1
+		maska <<= 1; 
+		maska += 1; 
 	}
-	unsigned b = a & maska; // Сохранил младщие L бит в b
-	b <<= (n - 3); // Сдвигаем младшие биты в начало блока
-	a >>= 3; // Сдвиг вправо на L бит
-	return a | b; // Получаем результат
+	unsigned b = a & maska; 
+	b <<= (n - 3);
+	a >>= 3; 
+	return a | b; 
 }
 
-unsigned left(unsigned a, int n) // Сдвиг влево на L бит, n - размер блока в битах
+unsigned left(unsigned a, int n) 
 {
-	// a- исходное значение
-	unsigned maska = 0; // Маска для получения левых L битов (старшие L разрядов должны быть 1)
+	unsigned maska = 0;
 	for (int i = 0; i<3; i++)
 	{
-		maska <<= 1; // Сдвиг влево на 1 бит
-		maska += 1; // Младший бит в 1
+		maska <<= 1; 
+		maska += 1;
 	}
-	maska <<= (n - 3); // Младшие биты сдвигаем в старшую часть блока
+	maska <<= (n - 3);
 
-	unsigned b = a & maska; // Сохранил старшие L бит в b
-	b >>= (n - 3); // Сдвигаем старшие биты в младшую часть
-	a <<= 3; // Сдвиг влево на L бит
-	return a | b; // Получаем результат
+	unsigned b = a & maska; 
+	b >>= (n - 3); 
+	a <<= 3; 
+	return a | b; 
 }
 
-// Одна функция шифрует и дешифрует (работает с блоками по 2 байта)
 void shifrDeshifr(char *in, char *out, int n, int key, int reg)
 {
-	// При шифровании сдвигаем на 2 бита влево 
-	/* in - входной массив
-	out - выходной массив
-	n - размерности массивов
-	key - секретный ключ
-	reg - режим работы функции 1 - режим шифрования 2 - режим расшифрования
-	*/
-	srand(key); // Инициализация генератора секретным ключом для получения гаммы
-				// Работаем с блоками по 2 байта
-	unsigned short a, b, gamma; // Входное и выходное значения
-	int l; // Размер блока в байтах
+	srand(key); 
+	unsigned short a, b, gamma; 
+	int l; 
 	for (int i = 0; i < n; i += 2)
 	{
-		if (n % 2 != 0 && i == n - 1) // Условие последний блок не полный - 1 байт
+		if (n % 2 != 0 && i == n - 1) 
 		{
 			a = 0;
 			l = 1;
 
 		}
 		else l = 2;
-		memcpy(&a, in + i, l); // Копируем l байта из входного массива
-		if (reg == 2) // Режим дешифрования
-			a = right(a, l * 8); // Сдвигаем на 2 бита вправо
+		memcpy(&a, in + i, l); 
+		if (reg == 2) 
+			a = right(a, l * 8); 
 		gamma = rand();
 		b = gamma ^ a;
-		if (reg == 1) b = left(b, l * 8); // Сдвигаем на 2 бита влево при шифровании
-		memcpy(out + i, &b, l); // Копируем l байта в выходной массив
+		if (reg == 1) b = left(b, l * 8); 
+		memcpy(out + i, &b, l);
 
 	}
 }
@@ -92,19 +81,17 @@ int main()
 	printf_s("\n n = %d", n);
 
 	srand(time(NULL));
-	gamma = rand() % 36000; /* 194 Для проверки на q */
+	gamma = rand() % 36000;
 	printf_s("\n gamma = %d ", gamma);
 
-	// Шифруем
+	
 	shifrDeshifr(str, strSh, n, gamma, 1);
-	// Печаем зашифрованный массив по символьно и коды (т.к. данные двоичные)
 	printf("\n After coding: \n%s\n", strSh);
 	
 
-	// Расшифруем
+	
 	shifrDeshifr(strSh, strDeSh, n, gamma, 2);
-	strDeSh[n] = 0; // Строка заканчивается 0
-					 // Печать строки после расшифровки
+	strDeSh[n] = 0;
 	printf("\n After decoding:\n%s\n", strDeSh);
 	system("pause");
 
